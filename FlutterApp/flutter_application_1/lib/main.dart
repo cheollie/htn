ba81a545160cae07b910b83015a7877b18220d34
Jacqueline Fung <fungjsl@gmail.com>
2023-09-16 21:47:10 -0400

    experimental stuff'

--- FlutterApp/flutter_application_1/lib/main.dart
+++ FlutterApp/flutter_application_1/lib/main.dart
@@ -1,12 +1,19 @@
+import 'package:flutter/widgets.dart';
+
 import 'package:flutter/material.dart';
 
 import 'src/app.dart';
 import 'src/settings/settings_controller.dart';
 import 'src/settings/settings_service.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
 void main() async {
   // Set up the SettingsController, which will glue user settings to multiple
   // Flutter Widgets.
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   final settingsController = SettingsController(SettingsService());
 
   // Load the user's preferred theme while the splash screen is displayed.
---
+++ app/src/main/java/com/example/fcmpushnotification/MyFirebaseMessagingService.kt
@@ -0,0 +1,4 @@
+package com.example.fcmpushnotification
+
+class MyFirebaseMessagingService {
+}
