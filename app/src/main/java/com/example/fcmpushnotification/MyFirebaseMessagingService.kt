6efd7e52387d92003844aa46e5ef188967ab444b
Jacqueline Fung <fungjsl@gmail.com>
2023-09-16 21:49:02 -0400

    experimental changes

--- app/src/main/java/com/example/fcmpushnotification/MyFirebaseMessagingService.kt
+++ app/src/main/java/com/example/fcmpushnotification/MyFirebaseMessagingService.kt
@@ -1,4 +1,22 @@
 package com.example.fcmpushnotification
 
-class MyFirebaseMessagingService {
+import android.app.PendingIntent
+import android.content.Intent
+import com.google.firebase.messaging.FirebaseMessagingService
+
+const val channelId = "notification_channel"
+const val channelName
+class MyFirebaseMessagingService : FirebaseMessagingService() {
+    fun generateNotification(title: String, message: String){
+        val intent = Intent(this,MainActivity::class.java)
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP)
+
+        val pendingIntent = PendingIntent.getActivity(this, 0, intent,
+            PendingIntent.FLAG_ONE_SHOT or PendingIntent.FLAG_IMMUTABLE)
+
+
+
+
+    }
+
 }
