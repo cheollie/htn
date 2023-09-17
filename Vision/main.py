b77ab0a9e0f13ab3432b9843601fe683e582a167
Chelsea W. <80985676+cheollie@users.noreply.github.com>
2023-09-17 08:02:04 -0400

    cohere

--- Vision/main.py
+++ Vision/main.py
@@ -1,11 +1,11 @@
 from Detector import *
 import os
 def main():
-    videoPath = "tcp://10.42.0.116:8888"
-    #videoPath = "Vision/test_videos/kitchen.mp4"
-    configPath = os.path.join("ssd_mobilenet_v3_large_coco_2020_01_14.pbtxt")
-    modelPath = os.path.join("frozen_inference_graph.pb")
-    classesPath = os.path.join("coco.names")
+    #videoPath = "tcp://10.42.0.116:8888"
+    videoPath = "Vision/test_videos/kitchen.mp4"
+    configPath = os.path.join("Vision","model_data","ssd_mobilenet_v3_large_coco_2020_01_14.pbtxt")
+    modelPath = os.path.join("Vision","model_data","frozen_inference_graph.pb")
+    classesPath = os.path.join("Vision","model_data","coco.names")
     detector = Detector (videoPath, configPath, modelPath, classesPath)
     detector.onVideo()
 if __name__ == '__main__' :
